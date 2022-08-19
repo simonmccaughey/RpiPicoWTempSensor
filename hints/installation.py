052527e9c9310df32d5221ed4e305aadf3a887d9
Simon McCaughey <simon.mccaughey@sita.aero>
2022-08-19 12:45:20 +0100

    initial working version

--- hints/installation.py
+++ hints/installation.py
@@ -2,4 +2,5 @@
 import upip
 upip.install('micropython-ulogging')
 upip.install('micropython-ssd1306')
+upip.install('picoweb')
 
--- hints/network.py
+++ hints/network.py
@@ -2,5 +2,5 @@ import network
 sta_if = network.WLAN(network.STA_IF)
 sta_if.active(True)
 sta_if.connect('BTWholeHome-CJW', '1029384756abc')
-ststa_if.ifconfig()
 sta_if.isconnected()
+sta_if.ifconfig()
