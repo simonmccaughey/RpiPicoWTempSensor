bcd0ff002df1ce7a1518625e78c2658bb239d5c5
Simon McCaughey <simon.mccaughey@sita.aero>
2022-08-31 07:57:22 +0100

    added printout every 10 iterations

--- watchdog.py
+++ watchdog.py
@@ -4,18 +4,21 @@ from machine import Timer
 
 class WDT:
   def __init__(self):
-    self.timer = Timer()  
-    #self.timer.init(period=1000, mode=Timer.PERIODIC, callback=self.do_check)  
     self.iteration = 0
     self.max = 60
+    
+    self.timer = Timer()  
+    self.timer.init(period=1000, mode=Timer.PERIODIC, callback=self.do_check)  
+    
   def setmax(self, max):
     self.max = max
 
   def do_check(self, arg):
     self.iteration += 1
     #print('Elapsed--->>> ' , self.iteration)
-    #if(self.iteration % 5 == 0):
-    #  print('WDT Iteration ', self.iteration)
+    if(self.iteration % 10 == 0):
+      print('WDT Iteration ', self.iteration)
+
     if self.iteration > self.max:
       print('watchdog reset...')
       machine.reset()
@@ -24,6 +27,7 @@ class WDT:
     #print('iteration = ' + str(self.iteration))
     self.iteration = 0
     #print('fed! ')
+
 
 
 
