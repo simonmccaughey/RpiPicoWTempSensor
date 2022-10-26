9db8be5fe233c7ea87409eca8fda9dcb2dbf52c4
Simon McCaughey <simon.mccaughey@sita.aero>
2022-10-26 15:16:37 +0100

    added example ap conf

--- hints/network.py
+++ hints/network.py
@@ -4,3 +4,13 @@ sta_if.active(True)
 sta_if.connect('BTWholeHome-CJW', '1029384756abc')
 sta_if.isconnected()
 sta_if.ifconfig()
+
+
+
+
+ap = network.WLAN(network.AP_IF)
+ap.active(False)
+id = f'Thermostat_XXX' 
+ap.config(ssid=id, essid=id, password = '1029384756')
+ap.active(True)
+
