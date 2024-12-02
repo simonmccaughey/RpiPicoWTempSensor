b877703dd3a7b1e2966669e3ed51a2e828edaf42
Simon McCaughey <simon.mccaughey@sita.aero>
2024-12-02 21:19:51 +0000

    networking updated

--- hints/network.py
+++ hints/network.py
@@ -1,7 +1,7 @@
 import network
 sta_if = network.WLAN(network.STA_IF)
 sta_if.active(True)
-sta_if.ifconfig(('0.0.0.0', '0.0.0.0', '0.0.0.0', '0.0.0.0'))
+#sta_if.ifconfig(('0.0.0.0', '0.0.0.0', '0.0.0.0', '0.0.0.0'))
 sta_if.connect('BTWholeHome-CJW', '1029384756abc')
 sta_if.isconnected()
 sta_if.ifconfig()
